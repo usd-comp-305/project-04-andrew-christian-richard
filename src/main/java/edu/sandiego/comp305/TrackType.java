983a6c81ad8540254a3e626fff4e3eaa8423f5e4
Richard Ngo <richardno@sandiego.edu>
2026-04-30 11:26:31 -0700

    EventFactory: created values for TrackType

--- src/main/java/edu/sandiego/comp305/TrackType.java
+++ src/main/java/edu/sandiego/comp305/TrackType.java
@@ -1,7 +1,13 @@
 package edu.sandiego.comp305;
 
 public enum TrackType {
-    ONE_HUNDRED_METER,
-    TWO_HUNDRED_METER,
-    FOUR_HUNDRED_METER
+    ONE_HUNDRED_METER(100),
+    TWO_HUNDRED_METER(200),
+    FOUR_HUNDRED_METER(400);
+
+    private final int length;
+
+    TrackType(int length) {
+        this.length = length;
+    }
 }
