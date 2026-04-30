cc57e357ecf960b2db694eaddf6aa09753f7c7e3
Richard Ngo <richardno@sandiego.edu>
2026-04-30 11:25:25 -0700

    EventFactory: created values for StaminaChange

--- src/main/java/edu/sandiego/comp305/StaminaChange.java
+++ src/main/java/edu/sandiego/comp305/StaminaChange.java
@@ -1,8 +1,14 @@
 package edu.sandiego.comp305;
 
 public enum StaminaChange {
-    COSTLY_LOST,
-    LOSS,
-    GAIN,
-    FREE,
+    COSTLY_LOST(-3),
+    LOSS(-1),
+    GAIN(1),
+    FREE(0);
+
+    private final int change;
+
+    StaminaChange(int change) {
+        this.change = change;
+    }
 }
