5a6ccafaa9f36e4d5a7bf496686aca19b1a92961
Richard Ngo <richardno@sandiego.edu>
2026-05-03 17:59:53 -0700

    AbstractEventFactoryTests: refactored, so it adjusted to the changes of the Event class.

--- src/test/java/edu/sandiego/comp305/AbstractEventFactoryTests.java
+++ src/test/java/edu/sandiego/comp305/AbstractEventFactoryTests.java
@@ -1,22 +1,30 @@
 package edu.sandiego.comp305;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class AbstractEventFactoryTests{
+    private List<String> mockDescriptions;
+    private Random random;
+
+    @BeforeEach
+    void init(){
+        List<String> mockDescriptions = List.of("Mock event description");
+        Random random = mock(Random.class);
+    }
     @Test
     void createRandomEvent_returnsNeutralEvent(){
-        Random random = mock(Random.class);
         when(random.nextInt(anyInt())).thenReturn(1);
 
-        AbstractEventFactory testFactory = new AbstractEventFactory();
+        AbstractEventFactory testFactory = new AbstractEventFactory(mockDescriptions, mockDescriptions, mockDescriptions);
         Event testEvent = testFactory.createRandomEvent(random);
 
         assertInstanceOf(NeutralEvent.class, testEvent);
@@ -24,10 +32,9 @@ public class AbstractEventFactoryTests{
 
     @Test
     void createRandomEvent_returnsGoodEvent(){
-        Random random = mock(Random.class);
         when(random.nextInt(anyInt())).thenReturn(70);
 
-        AbstractEventFactory testFactory = new AbstractEventFactory();
+        AbstractEventFactory testFactory = new AbstractEventFactory(mockDescriptions, mockDescriptions, mockDescriptions);
         Event testEvent = testFactory.createRandomEvent(random);
 
         assertInstanceOf(GoodEvent.class, testEvent);
@@ -35,10 +42,9 @@ public class AbstractEventFactoryTests{
 
     @Test
     void createRandomEvent_returnsBadEvent(){
-        Random random = mock(Random.class);
         when(random.nextInt(anyInt())).thenReturn(90);
 
-        AbstractEventFactory testFactory = new AbstractEventFactory();
+        AbstractEventFactory testFactory = new AbstractEventFactory(mockDescriptions, mockDescriptions, mockDescriptions);
         Event testEvent = testFactory.createRandomEvent(random);
 
         assertInstanceOf(BadEvent.class, testEvent);
