d53c13a150c40c950affb8d9e7f75a5fa616003c
Richard Ngo <richardno@sandiego.edu>
2026-05-03 17:48:36 -0700

    AbstractEventFactory: Created instance variables for each Event description (Good, Bad, Neutral)

--- src/main/java/edu/sandiego/comp305/AbstractEventFactory.java
+++ src/main/java/edu/sandiego/comp305/AbstractEventFactory.java
@@ -1,11 +1,25 @@
 package edu.sandiego.comp305;
 
+import java.util.List;
 import java.util.Random;
 
 public class AbstractEventFactory implements EventFactory{
     private static final int NEUTRAL_EVENT_THRESHOLD = 65;
     private static final int GOOD_EVENT_THRESHOLD = 85;
     private static final int FULL_PERCENTAGE = 100;
+
+    private final List<String> goodDescriptions;
+    private final List<String> neutralDescriptions;
+    private final List<String> badDescriptions;
+
+    public AbstractEventFactory(
+            List<String> goodDescriptions,
+            List<String> neutralDescriptions,
+            List<String> badDescriptions) {
+        this.goodDescriptions = goodDescriptions;
+        this.neutralDescriptions = neutralDescriptions;
+        this.badDescriptions = badDescriptions;
+    }
 
     public Event createRandomEvent(Random random) {
         int roll = RandomRoll(random);
