0cd263783b28c2f4c64690d30ed44bbadf246a6f
Richard Ngo <richardno@sandiego.edu>
2026-05-04 21:40:50 -0700

    Refactor: created a separate class to get descriptions, so AbstractEventFactory does not have to manage it.

---
+++ src/main/java/edu/sandiego/comp305/EventDescriptionProvider.java
@@ -0,0 +1,44 @@
+package edu.sandiego.comp305;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class EventDescriptionProvider {
+    private final Map<EventType, List<String>> descriptionsByType;
+
+    public EventDescriptionProvider() {
+        descriptionsByType = Map.of(
+                EventType.NEUTRAL, List.of(
+                        "Another horse is neck-to-neck to you",
+                        "Your horse is intimidated",
+                        "Your horse's stride remains the same",
+                        "Your horse is getting antsy"
+                ),
+                EventType.GOOD, List.of(
+                        "Your horse is in flow state!",
+                        "There is a clear gap!",
+                        "The crowd is cheering for you to win!",
+                        "Your horse's footing is perfect!",
+                        "Your horse's strides are getting faster!",
+                        "Your horse turned a corner perfectly!"
+                ),
+                EventType.BAD, List.of(
+                        "Your horse got startled!",
+                        "Your horse has lost its footing!",
+                        "Your horse got distracted!",
+                        "Your horse's breathing is out of control!",
+                        "Your horse just stumbled!",
+                        "Your horse is losing its pacing!"
+                )
+        );
+    }
+
+    public String getRandomDescription(EventType type, Random random) {
+        List<String> descriptions = descriptionsByType.get(type);
+
+        int randomIndex = random.nextInt(descriptions.size());
+        return descriptions.get(randomIndex);
+    }
+
+}
