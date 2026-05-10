c02a404a44dec368587bd69975890f0e4791a0b0
andrewjn18 <anuha@sandiego.edu>
2026-05-10 15:27:59 -0700

    feat: enhance HorseUpgradeSystem with dynamic stat upgrades and trophy rewards

--- src/main/java/edu/sandiego/comp305/HorseUpgradeSystem.java
+++ src/main/java/edu/sandiego/comp305/HorseUpgradeSystem.java
@@ -1,18 +1,45 @@
 package edu.sandiego.comp305;
 
 public class HorseUpgradeSystem implements UpgradeSystem {
+    private static final int LEVEL_UP_AMOUNT = 1;
+
     @Override
     public void levelUpHorse(Horse horse) {
-
+        horse.getStats().increaseSpeed(LEVEL_UP_AMOUNT);
+        horse.getStats().increaseStamina(LEVEL_UP_AMOUNT);
+        horse.getStats().increasePower(LEVEL_UP_AMOUNT);
     }
 
     @Override
     public void applyUpgrade(Horse horse, String statType, int amount) {
-
+        switch (statType.toLowerCase()) {
+            case "speed":
+                horse.getStats().increaseSpeed(amount);
+                break;
+            case "stamina":
+                horse.getStats().increaseStamina(amount);
+                break;
+            case "power":
+                horse.getStats().increasePower(amount);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid stat type: " + statType);
+        }
     }
 
     @Override
     public void awardTrophies(Horse horse, Placement placement) {
+        horse.addTrophies(getTrophyValuesByPlacement(placement));
+    }
 
+    private int getTrophyValuesByPlacement(Placement placement) {
+        return switch (placement) {
+            case FIRST -> 5;
+            case SECOND -> 4;
+            case THIRD -> 3;
+            case FOURTH -> 2;
+            case FIFTH -> 1;
+            default -> 0;
+        };
     }
 }
