06b93b6ecd3cbd93a0e56d80f320ea70b3c5f2c9
Richard Ngo <richardno@sandiego.edu>
2026-05-11 23:00:08 -0700

    Simulation: finished implementation of printEvent

--- src/main/java/edu/sandiego/comp305/Simulation.java
+++ src/main/java/edu/sandiego/comp305/Simulation.java
@@ -9,6 +9,7 @@ public class Simulation {
     private final HorseUpgradeSystem upgradeSystem;
     private final HorseFactory horseFactory;
     private Horse playerHorse;
+    private final static int NUM_CHOICES = 3;
 
     public Simulation(Scanner scanner, RaceManager raceManager, HorseUpgradeSystem upgradeSystem,
                      HorseFactory horseFactory) {
@@ -24,9 +25,21 @@ public class Simulation {
         List<EventChoice> choices = event.getEventChoices();
 
         System.out.println("══════════════════════════════════════════════════════");
-        System.out.printf("%-52s %n", event.getDescription());
+        System.out.printf("%-52s \n", event.getDescription());
         System.out.println("══════════════════════════════════════════════════════");
 
-        for (int i )
+        for (int i = 0; i < NUM_CHOICES; i++){
+            EventChoice choice = choices.get(i);
+            RaceEffect effect = choice.getEffect();
+            String statChanges = String.format("SPD%+d PWR%+d", effect.getSpeedChange(),  effect.getPowerChange());
+            System.out.printf("[%d] %-30s %s \n", i + 1, choice.getLabel(), statChanges);
+            System.out.printf("%-48s \n", choice.getLabel());
+        }
+        System.out.println("══════════════════════════════════════════════════════");
+        System.out.printf( "Your stamina: %-38d \n", player.getStats().getStamina());
+        System.out.println("══════════════════════════════════════════════════════");
+        System.out.print("Choose (1-3): ");
     }
+
+
 }
