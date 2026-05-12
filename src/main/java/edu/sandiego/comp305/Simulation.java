e3a13baab40af3d5c440cffb63b246588e674f91
Richard Ngo <richardno@sandiego.edu>
2026-05-11 22:39:17 -0700

    Simulation: initialized instance variables

--- src/main/java/edu/sandiego/comp305/Simulation.java
+++ src/main/java/edu/sandiego/comp305/Simulation.java
@@ -1,5 +1,19 @@
 package edu.sandiego.comp305;
 
-public class Simulation {
+import java.util.Scanner;
 
+public class Simulation {
+    private final Scanner scanner;
+    private final RaceManager raceManager;
+    private final HorseUpgradeSystem upgradeSystem;
+    private final HorseFactory horseFactory;
+    private Horse playerHorse;
+
+    public Simulation(Scanner scanner, RaceManager raceManager, HorseUpgradeSystem upgradeSystem,
+                     HorseFactory horseFactory) {
+        this.scanner       = scanner;
+        this.raceManager   = raceManager;
+        this.upgradeSystem = upgradeSystem;
+        this.horseFactory  = horseFactory;
+    }
 }
